90f95a6e0b2beb3ece97fe5b20618ba54d842fb5
italo pc casa <italohttp25@gmail.com>
2025-09-11 23:43:43 -0300

    construct

--- bd/Mysql.php
+++ bd/Mysql.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace db;
 
 use Exception;
@@ -8,14 +9,15 @@ require_once '../env.php';
 
 class Mysql
 {
+
     const ERR_DUPLICIDADE       = 1062;
     const ERR_CHAVE_ESTRANGEIRA = 1451;
 
-const HOST = $_ENV['DB_HOST'];
-const PORT = $_ENV['DB_PORT'];
-const BASE   = $_ENV['DB_DATABASE'];
-const USERNAME = $_ENV['DB_USERNAME'];
-const PASSWORD = $_ENV['DB_PASSWORD'];
+    const HOST = $_ENV['DB_HOST'];
+    const PORT = $_ENV['DB_PORT'];
+    const BASE   = $_ENV['DB_DATABASE'];
+    const USERNAME = $_ENV['DB_USERNAME'];
+    const PASSWORD = $_ENV['DB_PASSWORD'];
 
 
     public static function connection($dataSource = null): mysqli
--- model/User.php
+++ model/User.php
@@ -4,10 +4,61 @@ namespace model;
 
 use JsonSerializable;
 use Exception;
+use db\Mysql;
 use function json_decode;
+use function strlen;
 
 class User implements JsonSerializable
 {
-    private int $id
-    
+    const STATUS = [
+        1 => 'PENDING',
+        2 => 'ACTIVE',
+        3 => 'INACTIVE',
+    ];
+
+    private int $id;
+    private string $name;
+    private string $email;
+    private string $phone;
+    private $user;
+    private string $password;
+    private int $status;
+    private string $createdAt;
+    private string $updatedAt;
+
+    public function jsonSerialize(): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'phone' => $this->phone,
+            'user' => $this->user,
+            'status' => $this->status,
+            'createdAt' => $this->createdAt,
+            'updatedAt' => $this->updatedAt,
+        ];
+    }
+
+    public function __construct(int $id)
+    {
+        if ($id) {
+            $con = Mysql::connection();
+            $query = <<<QUERY
+            SELECT id,name,email,phone,user,status,created_at,updated_at
+            FROM usuario WHERE codigo = ?
+            QUERY;
+            $result = $con->execute_query($query, [$id]);
+            while ($row = $result->fetch_assoc()) {
+                $this->id = $row['id'];
+                $this->name = $row['name'];
+                $this->email = $row['email'];
+                $this->phone = $row['phone'];
+                $this->user = $row['user'];
+                $this->status = $row['status'];
+                $this->createdAt = $row['created_at'];
+                $this->updatedAt = $row['updated_at'];
+            }
+        }
+    }
 }
