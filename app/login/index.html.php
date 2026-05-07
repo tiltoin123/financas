0f7ebb2b69e1bab2f0131d8679dfd8e5e59673e9
tiltoin123 <italohttp25@gmail.com>
2026-05-07 15:47:52 -0300

    página de login

---
+++ app/login/index.html.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <link rel="stylesheet" href="./index.css">
+    <title></title>
+</head>
+<body>
+
+<form id="login-form">
+    <label>
+        <input id="email" type="text" name="email">
+    </label>
+    <label>
+        <input id="senha" type="password" name="senha">
+    </label>
+    <button id="entrar" type="button">Entrar</button>
+</form>
+
+<script src="./index.js"></script>
+
+</body>
+</html>
